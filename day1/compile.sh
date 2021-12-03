d8fcdd6082bd936b90a12268b62548fd73b48067
Balazs Eszes <c4deszes@gmail.com>
2021-12-03 22:58:20 +0100

    Debug flags

--- day1/compile.sh
+++ day1/compile.sh
@@ -1,4 +1,4 @@
 mkdir bin
-gcc -c program.S -o ./bin/program.o && ld ./bin/program.o -o ./bin/program
-cat data.txt | ./bin/program | hexyl
+gcc -Og -g -c program.S -o ./bin/program.o && ld ./bin/program.o -o ./bin/program
+cat test.txt | ./bin/program | hexyl
 # gdb -ex start --args ./a.out
